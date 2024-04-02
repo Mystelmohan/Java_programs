280975a578bbf39317551107199a3c806d131c4e
Mystelmohan <mohanrj7904@gmail.com>
2024-04-02 14:17:35 +0530

    updated

--- Pair_prime.java
+++ Pair_prime.java
@@ -1,5 +1,28 @@
+import java.util.*;
 public class Pair_prime {
     public static void main(String[] args) {
-        
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int count=0;
+        for(int i=a;i<=b;i++){
+            if(isprime(i)){
+                if(isprime(i+6)){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+    static boolean isprime(int n){
+        if(n==0 || n==1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
     }
 }
