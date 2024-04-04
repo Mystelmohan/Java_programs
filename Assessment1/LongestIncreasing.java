49e28b7284b2038f5a892bb9d9554977a843fdec
Mystelmohan <147577443+Mystelmohan@users.noreply.github.com>
2024-04-04 09:34:20 +0530

    add

---
+++ Assessment1/LongestIncreasing.java
@@ -0,0 +1,39 @@
+package Assessment1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LongestIncreasing {
+   
+    public static int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        int maxLength = Arrays.stream(dp).max().orElse(0);
+        return maxLength;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++){
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(lengthOfLIS(arr));
+        sc.close();
+    }
+}
+
---
+++ Assessment1/ReversedLL.java
@@ -0,0 +1,85 @@
+package Assessment1;
+
+import java.util.Scanner;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class List {
+    Node head, tail;
+
+    List() {
+        head = tail = null;
+    }
+
+    void insertAtBeggining(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            tail = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    void insertAtEnd(int data) {
+        if (tail == null) {
+            insertAtBeggining(data);
+        } else {
+            Node newNode = new Node(data);
+            tail.next = newNode;
+            tail = newNode;
+        }
+       
+    }
+
+    Node reverse(Node head, int k) {
+        if (head == null)
+            return head;
+        Node currNode = head, prevNode = null, nextNode = null;
+        int count = 0;
+        while (count < k && currNode != null) {
+            nextNode = currNode.next;
+            currNode.next = prevNode;
+            prevNode = currNode;
+            currNode = nextNode;
+            count++;
+        }
+        if (nextNode != null) {
+            head.next = reverse(nextNode, k);
+        }
+        return prevNode;
+    }
+
+    void traverse() {
+        System.out.println("The List is: ");
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.print("null\n");
+    }
+}
+
+public class ReversedLL {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List l = new List();
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            l.insertAtEnd(in.nextInt());
+        }
+        int k = in.nextInt();
+        l.traverse();
+        l.head = l.reverse(l.head, k);
+        l.traverse();
+        in.close();
+    }
+}
